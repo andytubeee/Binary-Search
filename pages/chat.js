808ace8bb38de1bfb3d3462c3255b6633aa5d63d
Andrew Yang <andy.tubeee@gmail.com>
2022-04-17 16:26:37 -0400

    chat page style update

--- pages/chat.js
+++ pages/chat.js
@@ -2,6 +2,7 @@ import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { getSession } from 'next-auth/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { AccountSettings } from '../components/AccountSettings';
 import Navbar from '../components/Navbar';
@@ -9,13 +10,15 @@ import { getOtherUsers, getUserByEmail } from '../utils/backend/getUser';
 
 export default function ChatPage({ pageProps }) {
   const { session, notConfirmed, user } = pageProps;
-  const ChatWindow = (userId) => {};
+  const router = useRouter();
+  const ChatWindow = ({ userId }) => {
+    if (!userId) return null;
+  };
   const ChatUserColumn = () => {
     return (
       <div className='p-3 flex flex-col'>
         <div className='pb-2 border-b-2 flex flex-col'>
-          <button className='btn-blue w-100'>
-            {' '}
+          <button className='btn-blue w-100' onClick={() => router.push('/')}>
             <FontAwesomeIcon icon={faUserAlt} /> &nbsp; Find a User
           </button>
         </div>
@@ -27,23 +30,25 @@ export default function ChatPage({ pageProps }) {
       <Head>
         <title>Binary Search - Chat</title>
       </Head>
-      <Navbar signedIn={session} />
-      {notConfirmed ? (
-        <AccountSettings session={session.user} />
-      ) : (
-        <>
-          <h1 className='text-center text-3xl font-bold mt-5'>Chat</h1>
+      <div className='overflow-hidden min-h-screen flex flex-col'>
+        <Navbar signedIn={session} />
+        {notConfirmed ? (
+          <AccountSettings session={session.user} />
+        ) : (
+          <>
+            <h1 className='text-center text-3xl font-bold mt-5'>Chat</h1>
 
-          <div className='flex gap-3 h-screen justify-between px-4 border'>
-            <div className='flex border flex-col flex-[0.2]'>
-              <ChatUserColumn />
+            <div className='flex gap-3 justify-between px-4 my-3 flex-1'>
+              <div className='flex border flex-col flex-[0.2]'>
+                <ChatUserColumn />
+              </div>
+              <div className='flex flex-1 border flex-col'>
+                <ChatWindow />
+              </div>
             </div>
-            <div className='flex flex-1 border flex-col'>
-              <ChatWindow />
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </>
   );
 }
